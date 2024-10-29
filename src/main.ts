613ed13ba2340975f0da609f78ae761cd594c45c
Samuel Elie <selie@anduril.com>
2024-10-28 19:55:44 -0700

    uno

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { join, parse } from "node:path";
+import { fileURLToPath } from "node:url";
+import dotenv from "dotenv-flow";
+import { pkgUpSync } from "pkg-up";
+
+const dirname = parse(fileURLToPath(import.meta.url)).dir;
+export const pkgRootDir = parse(pkgUpSync({ cwd: dirname }) || "").dir;
+
+const p = join(pkgRootDir, "../../", ".config");
+dotenv.config({ path: p });
+
+export function getEnv(k: string) {
+    // eslint-disable-next-line node/prefer-global/process
+    return process.env[k];
+}
+
+export const isProd = getEnv("NODE_ENV") === "production";
---
+++ src/types.d.ts
@@ -0,0 +1,15 @@
+interface ImportMeta {
+  env: {
+    MORTAR_PORT: string
+    POSTGRES_HOST: string
+    POSTGRES_USER: string
+    POSTGRES_PASSWORD: string
+    POSTGRES_DB: string
+    POSTGRES_PORT: string
+    VITE_SERVER_HOST: string
+    VITE_DEV_SERVER_PORT: string
+    VITE_SERVER_PORT: string
+    K8_NAMESPACE: string
+
+  }
+}
